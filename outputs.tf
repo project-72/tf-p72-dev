04b18143a9f7effba604fcd8196c3cac6e1c2276
Simon Jones <sj.7272@gmail.com>
2021-10-10 13:13:14 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "rg_basic" {
   value = azurerm_resource_group.rg-basic.name
 }
 
-output "rg_storage" {
+output "st_storage" {
   value = azurerm_storage_account.st-basic.name
+}
+
+output "rnd" {
+  value = random_id.rnd
 }
