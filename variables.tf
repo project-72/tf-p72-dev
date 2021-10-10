cfd3039e48e56e2b8dcade69ab99faefcf5de6b3
Simon Jones <sj.7272@gmail.com>
2021-10-10 10:05:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,9 +8,12 @@ variable "project_name" {
   }
 }
 
-variable "location" {
-    description = "Azure region"
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
 
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
 }
 
 variable "env" {
