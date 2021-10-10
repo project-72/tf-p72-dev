158a1d625af9eca2332ac84199f0d5754d11e5fb
Simon Jones <sj.7272@gmail.com>
2021-10-10 10:18:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "=2.80.0"
     }
   }
 }
@@ -13,15 +13,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
+resource "azurerm_resource_group" "example-rg" {
+  name     = "${var.prefix}-rg"
   location = "${var.location}"
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "${var.prefix}storageacct"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.example-rg.name
+  location                 = azurerm_resource_group.example-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
